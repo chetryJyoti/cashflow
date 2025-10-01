973c97d9aa8b1c3b9a92d848ab03e7f5f044356f
jyoti chetry <jyotichetry087@gmail.com>
2025-10-01 14:33:47 +0530

    amount validations

--- tracker/forms.py
+++ tracker/forms.py
@@ -6,6 +6,13 @@ class TransactionForm(forms.ModelForm):
         queryset=Category.objects.all(),
         widget=forms.RadioSelect()
     )
+    
+    def clean_amount(self):
+        amount = self.cleaned_data["amount"]
+        if amount <=0:
+            raise forms.ValidationError("Amount must be greater than 0")
+        return amount
+    
     class Meta:
         model = Transaction
         fields = (
--- tracker/views.py
+++ tracker/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from tracker.models import Transaction
 from tracker.filters import TransactionFilter
 from tracker.forms import TransactionForm
+from django_htmx.http import retarget
 
 def index(request):
     return render(request, 'tracker/index.html')
@@ -36,6 +37,10 @@ def create_transaction(request):
             transaction.save()
             context = {"message":"Transaction added successfully."}
             return render(request,'tracker/partials/transaction-success.html', context)
+        else: 
+            context = {"form" : form}
+            response =  render(request,'tracker/partials/create-transaction.html', context)
+            return retarget(response, "#transaction-block")
             
     context={'form' : TransactionForm()}
     return render(request,'tracker/partials/create-transaction.html', context)
